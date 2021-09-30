63f36f3eaa278507bca74cd829b68f1a1cd70830
taki-21 <taki.it.0208.21@gmail.com>
2021-09-30 22:03:01 +0900

    Add: CompanyModel-関連付け追加

--- server/app/models/company.rb
+++ server/app/models/company.rb
@@ -1,5 +1,10 @@
 class Company < ApplicationRecord
   has_one_attached :company_image
+  has_many :users
+  has_many :techstacks, dependent: :destroy
+  has_many :incomes, dependent: :destroy
+  has_many :welfares, dependent: :destroy
+  has_many :selections, dependent: :destroy
   validates :company_name, presence: true, length: {maximum: 255}, uniqueness: true
   validates :company_address, presence: true, length: {maximum: 300}
   validates :company_overview, presence: true, length: {maximum: 2000}
