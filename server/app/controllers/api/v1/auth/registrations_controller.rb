b788bd86006e42bbd2202e2608d6edeed58bdc95
藤原彰人 <1998922akito@gmail.com>
2021-10-02 21:15:26 +0900

    terraform の state管理をとコンテナ環境を作成

--- server/app/controllers/api/v1/auth/registrations_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
-  private
-
-    def sign_up_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :name)
-    end
-end
--- server/app/controllers/application_controller.rb
+++
@@ -1,6 +0,0 @@
-class ApplicationController < ActionController::Base
-  include DeviseTokenAuth::Concerns::SetUserByToken
-
-  skip_before_action :verify_authenticity_token
-  helper_method :current_user, :user_signed_in?
-end
