befa28a84524341401ba2f7ce8c914244897f17e
taki-21 <taki.it.0208.21@gmail.com>
2021-10-06 02:06:09 +0900

    Add: CompanyTech, Techcategoryのアソシエーションの追加

--- server/app/models/company.rb
+++ server/app/models/company.rb
@@ -5,6 +5,10 @@ class Company < ApplicationRecord
   has_many :incomes, dependent: :destroy
   has_many :welfares, dependent: :destroy
   has_many :selections, dependent: :destroy
+  has_many :company_teches
+  has_many :techcategories, through: :company_teches
+
+
   validates :company_name, presence: true, length: {maximum: 255}, uniqueness: true
   validates :company_address, presence: true, length: {maximum: 300}
   validates :company_overview, presence: true, length: {maximum: 2000}
--- server/app/models/techcategory.rb
+++ server/app/models/techcategory.rb
@@ -1,2 +1,4 @@
 class Techcategory < ApplicationRecord
+  has_many :company_teches
+  has_many :companies, through: :company_teches
 end
