8e9969cc50f95e52c2516325397a079657988a6c
Hiroki1112 <pystdy@yahoo.co.jp>
2021-11-15 15:33:25 +0900

    ADD:言語による会社検索の実装

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/recommended-companies',to: 'companies#pickup'
+      get '/skill-search', to: 'companies#skillSearch'
       resources :companies do
         collection do
           get :search
