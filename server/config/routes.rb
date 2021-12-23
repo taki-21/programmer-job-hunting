fdb699603feb058a3c825ebc734d45f2abab9a48
taki-21 <taki.it.0208.21@gmail.com>
2021-12-24 01:30:57 +0900

    Add: ルーティングの設定#4

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
         collection do
           get :search
         end
+        resources :likes
       end
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations'
