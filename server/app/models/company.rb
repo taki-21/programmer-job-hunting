5918dd215eff68625f20b8e30d32ab7ef23e8dff
taki-21 <taki.it.0208.21@gmail.com>
2021-12-26 23:42:40 +0900

    Add: 会社ごとのいいね情報を返却する処理の追加#4

--- server/app/models/company.rb
+++ server/app/models/company.rb
@@ -9,25 +9,24 @@ class Company < ApplicationRecord
   has_many :techcategories, through: :company_teches
   has_many :likes
 
-
-  validates :company_name, presence: true, length: {maximum: 255}, uniqueness: true
-  validates :company_address, presence: true, length: {maximum: 300}
-  validates :company_overview, presence: true, length: {maximum: 2000}
+  validates :company_name, presence: true, length: { maximum: 255 }, uniqueness: true
+  validates :company_address, presence: true, length: { maximum: 300 }
+  validates :company_overview, presence: true, length: { maximum: 2000 }
 
   def self.search(search)
-      if search
-        Company.where('company_name LIKE(?)', "%#{search}%")
-      else
-        Company.all
-      end
+    if search
+      Company.where('company_name LIKE(?)', "%#{search}%")
+    else
+      Company.all
     end
+  end
 
   # まずはtechCategoryテーブルから該当する技術のidを取得
   # -> 中間テーブルからそのスキルを持つ会社のIDを取得
   # -> 会社のリストを返却する
   def self.skillSearch(skill)
     if skill != ''
-      Techcategory.join(:company_teches).join(:companies).where(['category_name','%#{skill}%'])
+      Techcategory.join(:company_teches).join(:companies).where(['category_name', '%#{skill}%'])
     else
       Company.per(10).order(created_at: :desc)
     end
