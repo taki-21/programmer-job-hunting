9103c43c6e11ae3d98c2b7d2bc00b08c44ae8c22
taki-21 <taki.it.0208.21@gmail.com>
2022-01-17 03:09:51 +0900

    fix: お気に入り操作をcurrent_userを用いた実装に修正#18

--- server/app/controllers/api/v1/likes_controller.rb
+++ server/app/controllers/api/v1/likes_controller.rb
@@ -3,17 +3,28 @@
 module Api
   module V1
     class LikesController < ApplicationController
+      before_action :authenticate_api_v1_user!, only: %i[create destroy]
       def index
         likes = Like.where(company_id: params[:company_id])
         render json: likes
       end
 
       def create
-        Like.create(user_id: params[:user_id], company_id: params[:company_id])
+        like = Like.new(user_id: current_api_v1_user.id, company_id: params[:company_id])
+        if like.save
+          render json: { status: :ok, id: like.id }
+        else
+          render json: { status: :bad_request, errors: like.errors.messages }
+        end
       end
 
       def destroy
-        Like.find_by(user_id: params[:user_id], company_id: params[:company_id]).destroy
+        like = Like.find_by(user_id: current_api_v1_user.id, company_id: params[:company_id])
+        if like.destroy
+          render json: { status: :ok, id: like.id }
+        else
+          render json: { status: :bad_request, errors: like.errors.messages }
+        end
       end
     end
   end
