87f020a2ca4718c2987b8506f8726053a4074dd8
Hiroki1112 <pystdy@yahoo.co.jp>
2022-01-21 21:08:35 +0900

    fix : お気に入りのエンドポイントの修正 #16

--- client/app/src/lib/api/like.tsx
+++ client/app/src/lib/api/like.tsx
@@ -1,9 +1,9 @@
 import client from "./client";
 
 export const addLike = (conpanyId: number, userId: number) => {
-  return client.get(`companies/${conpanyId}/likes/${userId}`);
+  return client.post(`companies/${conpanyId}/likes`);
 }
 
 export const removeLike = (conpanyId: number, userId: number) => {
-  return client.delete(`companies/${conpanyId}/likes/${userId}`);
+  return client.delete(`companies/${conpanyId}/likes`);
 }
