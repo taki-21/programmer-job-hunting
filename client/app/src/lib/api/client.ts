1c7c98184b7a8a140ae458a21a505cbae0a342a8
Hiroki1112 <pystdy@yahoo.co.jp>
2022-01-21 16:43:34 +0900

    fix : APIを叩く際に、認証情報を持っている場合はheaderに追加する #19
    Cookie内に情報を保存しているようなので、認証している時はCookieから取り出して追加するようにしました。

--- client/app/src/lib/api/client.ts
+++ client/app/src/lib/api/client.ts
@@ -1,5 +1,6 @@
 import applyCaseMiddleware from "axios-case-converter"
 import axios from "axios"
+import Cookies from "js-cookie"
 
 // applyCaseMiddleware:
 // axiosで受け取ったレスポンスの値をスネークケース→キャメルケースに変換
@@ -13,5 +14,19 @@ const options = {
 const client = applyCaseMiddleware(axios.create({
   baseURL: "http://localhost:3001/api/v1"
 }), options)
+
+client.interceptors.request.use(
+  config => {
+    if (Cookies.get("_access_token") && Cookies.get("_client") && Cookies.get("_uid")){
+      config.headers.common["access_token"] = Cookies.get("_access_token");
+      config.headers.common["client"] = Cookies.get("_client");
+      config.headers.common["uid"] = Cookies.get("_uid");
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 export default client
