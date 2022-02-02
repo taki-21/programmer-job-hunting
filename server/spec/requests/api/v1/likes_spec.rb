40d10220b47a83948121612f2046d981bf3f7150
taki-21 <taki.it.0208.21@gmail.com>
2022-02-03 00:55:45 +0900

    add: Like APIの削除テストの追加#15

--- server/spec/requests/api/v1/likes_spec.rb
+++ server/spec/requests/api/v1/likes_spec.rb
@@ -35,5 +35,24 @@ RSpec.describe "Api::V1::Likes", type: :request do
       # 正しい数のデータが返されたかを確認する
       expect(data.length).to eq(1)
     end
+
+    it "お気に入りの削除" do
+      # ログイン
+      user = FactoryBot.create(:user)
+      login user
+
+      # 会社1件新規投稿
+      company = FactoryBot.create(:company)
+      post "/api/v1/companies/#{company.id}/likes"
+      delete "/api/v1/companies/#{company.id}/likes"
+
+      data = JSON.parse(response.body)
+
+      # リクエスト成功を表す200が返ってきたかを確認する
+      expect(response.status).to eq(200)
+
+      expect(data['user_id']).to eq(user.id)
+      expect(data['company_id']).to eq(company.id)
+    end
   end
 end
