3b9e4c53c269e4e057b85151ae17936b2af4645a
taki-21 <taki.it.0208.21@gmail.com>
2022-02-03 00:52:33 +0900

    add: Like APIの一覧取得テストの追加#15

--- server/spec/requests/api/v1/likes_spec.rb
+++ server/spec/requests/api/v1/likes_spec.rb
@@ -17,5 +17,23 @@ RSpec.describe "Api::V1::Likes", type: :request do
       expect(data['user_id']).to eq(user.id)
       expect(data['company_id']).to eq(company.id)
     end
+
+    it "会社を指定した会社に登録されたお気に入り一覧を取得" do
+      # ログイン
+      user = FactoryBot.create(:user)
+      login user
+
+      # 会社1件新規投稿
+      company = FactoryBot.create(:company)
+      post "/api/v1/companies/#{company.id}/likes"
+      get "/api/v1/companies/#{company.id}/likes"
+      data = JSON.parse(response.body)
+
+      # リクエスト成功を表す200が返ってきたかを確認する
+      expect(response.status).to eq(200)
+
+      # 正しい数のデータが返されたかを確認する
+      expect(data.length).to eq(1)
+    end
   end
 end
