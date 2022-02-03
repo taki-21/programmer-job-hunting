c76eceaea7d74953098a5ab7d60f3c7dbb6485a8
taki-21 <taki.it.0208.21@gmail.com>
2022-02-03 22:21:45 +0900

    update: factoriesファイルの修正#15

--- server/spec/factories/users.rb
+++ server/spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
+    name {Faker::Name.name}
     sequence(:email) { |n| "test#{n}@example.com" }
-    name {'yamada'}
     password {'password'}
-    password_confirmation {'password'}
+    password_confirmation {Faker::Internet.password(8)}
   end
 end
