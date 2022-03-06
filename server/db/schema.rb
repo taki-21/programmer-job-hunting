2a87cfabbc095446002439b9833151c712167b07
taki-21 <taki.it.0208.21@gmail.com>
2022-03-07 02:35:27 +0900

    add: Roleテーブルの追加#26

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,141 +10,156 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_223_153_711) do
-  create_table 'active_storage_attachments', charset: 'utf8mb3', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'record_type', null: false
-    t.bigint 'record_id', null: false
-    t.bigint 'blob_id', null: false
-    t.datetime 'created_at', null: false
-    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
-    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
+ActiveRecord::Schema.define(version: 2022_03_06_171104) do
+
+  create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table 'active_storage_blobs', charset: 'utf8mb3', force: :cascade do |t|
-    t.string 'key', null: false
-    t.string 'filename', null: false
-    t.string 'content_type'
-    t.text 'metadata'
-    t.bigint 'byte_size', null: false
-    t.string 'checksum', null: false
-    t.datetime 'created_at', null: false
-    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  create_table "active_storage_blobs", charset: "utf8mb3", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.string "service_name", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table 'companies', charset: 'utf8mb3', force: :cascade do |t|
-    t.string 'company_name'
-    t.text 'company_address'
-    t.text 'company_overview'
-    t.string 'company_num_of_emp'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id'
-    t.index ['user_id'], name: 'index_companies_on_user_id'
+  create_table "active_storage_variant_records", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table 'company_teches', charset: 'utf8mb3', force: :cascade do |t|
-    t.bigint 'company_id', null: false
-    t.bigint 'techcategory_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['company_id'], name: 'index_company_teches_on_company_id'
-    t.index ['techcategory_id'], name: 'index_company_teches_on_techcategory_id'
+  create_table "companies", charset: "utf8mb3", force: :cascade do |t|
+    t.string "company_name"
+    t.text "company_address"
+    t.text "company_overview"
+    t.string "company_num_of_emp"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_companies_on_user_id"
   end
 
-  create_table 'incomes', charset: 'utf8mb3', force: :cascade do |t|
-    t.integer 'company_id', null: false
-    t.integer 'user_id', null: false
-    t.integer 'income', null: false
-    t.string 'content', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "company_teches", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "company_id", null: false
+    t.bigint "techcategory_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_company_teches_on_company_id"
+    t.index ["techcategory_id"], name: "index_company_teches_on_techcategory_id"
   end
 
-  create_table 'likes', charset: 'utf8mb3', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.bigint 'company_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['company_id'], name: 'index_likes_on_company_id'
-    t.index ['user_id'], name: 'index_likes_on_user_id'
+  create_table "incomes", charset: "utf8mb3", force: :cascade do |t|
+    t.integer "company_id", null: false
+    t.integer "user_id", null: false
+    t.integer "income", null: false
+    t.string "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'selections', charset: 'utf8mb3', force: :cascade do |t|
-    t.integer 'company_id', null: false
-    t.integer 'user_id', null: false
-    t.integer 'selection_category', null: false
-    t.string 'content', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "likes", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "company_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_likes_on_company_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table 'techcategories', charset: 'utf8mb3', force: :cascade do |t|
-    t.string 'category_name', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "roles", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'techstack_categories', charset: 'utf8mb3', force: :cascade do |t|
-    t.bigint 'techstack_id', null: false
-    t.bigint 'techcategory_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['techcategory_id'], name: 'index_techstack_categories_on_techcategory_id'
-    t.index ['techstack_id'], name: 'index_techstack_categories_on_techstack_id'
+  create_table "selections", charset: "utf8mb3", force: :cascade do |t|
+    t.integer "company_id", null: false
+    t.integer "user_id", null: false
+    t.integer "selection_category", null: false
+    t.string "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'techstacks', charset: 'utf8mb3', force: :cascade do |t|
-    t.integer 'company_id', null: false
-    t.integer 'user_id', null: false
-    t.integer 'tech_category', null: false
-    t.string 'content', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "techcategories", charset: "utf8mb3", force: :cascade do |t|
+    t.string "category_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users', charset: 'utf8mb3', force: :cascade do |t|
-    t.string 'provider', default: 'email', null: false
-    t.string 'uid', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.boolean 'allow_password_change', default: false
-    t.datetime 'remember_created_at'
-    t.string 'confirmation_token'
-    t.datetime 'confirmed_at'
-    t.datetime 'confirmation_sent_at'
-    t.string 'unconfirmed_email'
-    t.string 'name'
-    t.integer 'company_id'
-    t.string 'newgra_or_midcar'
-    t.string 'eng_category'
-    t.boolean 'admin', default: false
-    t.string 'image'
-    t.string 'email'
-    t.text 'tokens'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
-    t.index %w[uid provider], name: 'index_users_on_uid_and_provider', unique: true
+  create_table "techstack_categories", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "techstack_id", null: false
+    t.bigint "techcategory_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["techcategory_id"], name: "index_techstack_categories_on_techcategory_id"
+    t.index ["techstack_id"], name: "index_techstack_categories_on_techstack_id"
   end
 
-  create_table 'welfares', charset: 'utf8mb3', force: :cascade do |t|
-    t.integer 'company_id', null: false
-    t.integer 'user_id', null: false
-    t.string 'content', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "techstacks", charset: "utf8mb3", force: :cascade do |t|
+    t.integer "company_id", null: false
+    t.integer "user_id", null: false
+    t.integer "tech_category", null: false
+    t.string "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'companies', 'users'
-  add_foreign_key 'company_teches', 'companies'
-  add_foreign_key 'company_teches', 'techcategories'
-  add_foreign_key 'likes', 'companies'
-  add_foreign_key 'likes', 'users'
-  add_foreign_key 'techstack_categories', 'techcategories'
-  add_foreign_key 'techstack_categories', 'techstacks'
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "name"
+    t.integer "company_id"
+    t.string "newgra_or_midcar"
+    t.string "eng_category"
+    t.boolean "admin", default: false
+    t.string "image"
+    t.string "email"
+    t.text "tokens"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  end
+
+  create_table "welfares", charset: "utf8mb3", force: :cascade do |t|
+    t.integer "company_id", null: false
+    t.integer "user_id", null: false
+    t.string "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "companies", "users"
+  add_foreign_key "company_teches", "companies"
+  add_foreign_key "company_teches", "techcategories"
+  add_foreign_key "likes", "companies"
+  add_foreign_key "likes", "users"
+  add_foreign_key "techstack_categories", "techcategories"
+  add_foreign_key "techstack_categories", "techstacks"
 end
