9464542182309a4fbc2434b3ff61e5ef25b375c7
taki-21 <taki.it.0208.21@gmail.com>
2022-03-13 04:37:44 +0900

    update: Companyリソースのポリシーの修正#26

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -7,4 +7,17 @@ class ApplicationController < ActionController::API
   def pundit_user
     current_api_v1_user
   end
+
+  # rescue_from Pundit::NotAuthorizedError, with: :render_403
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  protected
+
+  def user_not_authorized
+    render status: 403, json: { message: "You don't have permission." }
+  end
+
+  # def render_403
+  #   render status: 403, json: { message: "You don't have permission." }
+  # end
 end
--- server/app/policies/company_policy.rb
+++ server/app/policies/company_policy.rb
@@ -1,11 +1,36 @@
 class CompanyPolicy < ApplicationPolicy
+  def pickup?
+    true
+  end
+  def index?
+    true
+  end
+  def search?
+    true
+  end
+  def skill_search?
+    true
+  end
+
+  def show?
+    true
+  end
+
   def create?
-    user.admin? or user.recruiter?
+    @user.present? && !@user.recruiter
   end
+
   def update?
-    user.admin? or user.recruiter?
+    @record.user == @user && !@user.recruiter
   end
+
   def destroy?
-    user.admin?
+    @record.user == @user && !@user.recruiter
+  end
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
   end
 end
