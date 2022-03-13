4fc793208312888788f41822eb93979adcb69e69
taki-21 <taki.it.0208.21@gmail.com>
2022-03-14 02:16:23 +0900

    update: Likeリソースのポリシーの修正#26

--- server/app/policies/like_policy.rb
+++ server/app/policies/like_policy.rb
@@ -1,9 +1,19 @@
 class LikePolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
   def create?
-    user.recruiter?
+    @user.present? && !@user.recruiter
   end
 
   def destroy?
-    user.recruiter?
+    @user.present? && !@user.recruiter
+  end
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
   end
 end
